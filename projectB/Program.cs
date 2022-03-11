3055286b724e7e27d81888dd39bb426ae83df36d
MustacheAltun <51758662+MustacheAltun@users.noreply.github.com>
2022-03-11 23:35:46 +0100

    Revert "Revert "registratie backend gemaakt""
    This reverts commit 56d48dfefec5edfe08f302d9855e29c7111d9943.

--- projectB/Program.cs
+++ projectB/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace projectB
 {
@@ -6,7 +10,30 @@ namespace projectB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //instantiate de class
+            Registratie accountMaken = new Registratie();
+
+            // geef de url van de json
+            string url = "..\\..\\..\\account.json";
+
+            // lees de file en zet alles in een string
+            string strResultJson = File.ReadAllText(url);
+
+            // maak een lijst van alle informatie die er is
+            List<Account> jsonList = JsonConvert.DeserializeObject<List<Account>>(strResultJson);
+
+            //voeg een lijst van tickets toe als je dat wilt, het hoeft niet
+            Ticket[] tickelijst = new Ticket[]
+            {
+                new Ticket() { id = 3, name = "Red" },
+                new Ticket() { id = 4, name = "Black" },
+                new Ticket() { id = 5, name = "Yellow" }
+            };
+
+            //geef alles naar de registreer funtie
+            //note: ticketlijst heeft een default value, dus je kan die leeg laten als je wilt
+            //note: als je dit nog een keer runned maakt hij een account, als dit niet wilt dan moet je lijn 36 commenten
+            accountMaken.Registreren(url, jsonList, "werk", "niet", "secreto", tickelijst);
         }
     }
 }
