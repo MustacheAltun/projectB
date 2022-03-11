d4bf9bc3f0f21388dfc075db219d53c700791df9
mustafa Altun <1034323@hr.nl>
2022-03-11 18:26:39 +0100

    registratie backend gemaakt

---
+++ projectB/registreer.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Registratie
+{
+    public void Registreren(string url, List<Account> accountList, string name, string password, string secret, Ticket[] tickets = null)
+	{
+        //voeg de nieuwe informatie toe aan de bestaande lijst van de parameter
+        accountList.Add(new Account()
+        {
+            username = name,
+            password = password,
+            security = secret,
+            tickets = tickets
+        });
+        //verdander de lijst naar een json type
+        var convertedJson = JsonConvert.SerializeObject(accountList, Formatting.Indented);
+        //verander de hele file met de nieuwe json informatie
+        File.WriteAllText(url, convertedJson);
+    }
+}
