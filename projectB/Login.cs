b6ab7005110971fee1193535fa1582268a4a97ee
Markito-debug <martinamarc40@gmail.com>
2022-03-13 15:34:01 +0100

    Login Class
    Ik heb login class gemaakt met inlog method. Ik heb ook een paar aanpassingen gemaakt bij json file en program.cs .

---
+++ projectB/Login.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Login
+{
+    public bool loginMethod (List<Account> accountList, string username, string password)
+    {
+        //Boolean om inlogstatus te checken.
+        bool loggedIn = false;
+        
+        //Itereren door elke item van Json list.
+        foreach(dynamic item in accountList)
+        {
+            //Check of gebruikersnaam en wachtwoord matchen
+            if(item.username.ToLower() == username.ToLower() && item.password == password)
+               loggedIn = true;
+        }
+        return loggedIn;
+        
+        
+    }
+}
--- projectB/Program.cs
+++ projectB/Program.cs
@@ -10,8 +10,16 @@ namespace projectB
     {
         static void Main(string[] args)
         {
+            string buttonBanner;
+            string username = "Kawish";
+            string password = "123456";
+            bool loggedInStatus = false;
+
             //instantiate de class
             Registratie accountMaken = new Registratie();
+
+            //object gemaakt van class Login
+            Login accountInloggen = new Login();
 
             // geef de url van de json
             string url = "..\\..\\..\\account.json";
@@ -30,10 +38,15 @@ namespace projectB
                 new Ticket() { id = 5, name = "Yellow" }
             };
 
+            //Als je dit roept ga je checken of de naam en wachtwoord die je hebt ingevoerd komt voor in Json file.
+            //Console.WriteLine(accountInloggen.loginMethod(jsonList, username, password));
+
             //geef alles naar de registreer funtie
             //note: ticketlijst heeft een default value, dus je kan die leeg laten als je wilt
             //note: als je dit nog een keer runned maakt hij een account, als dit niet wilt dan moet je lijn 36 commenten
-            accountMaken.Registreren(url, jsonList, "werk", "niet", "secreto", tickelijst);
+            //accountMaken.Registreren(url, jsonList, "werk", "niet", "secreto", tickelijst);
+
+
         }
     }
 }
