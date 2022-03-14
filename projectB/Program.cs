676d71fa7627c903c402b9afd3a3f2ac48d19772
KawishGangadin <100194278+KawishGangadin@users.noreply.github.com>
2022-03-14 22:59:59 +0100

    Hoofscherm Met Input
    Deze versie van de hoofscherm checkt met die boolean of je input correct is en returned dan de scherm naam.

--- projectB/Program.cs
+++ projectB/Program.cs
@@ -34,10 +34,8 @@ namespace projectB
             //note: ticketlijst heeft een default value, dus je kan die leeg laten als je wilt
             //note: als je dit nog een keer runned maakt hij een account, als dit niet wilt dan moet je lijn 36 commenten
             accountMaken.Registreren(url, jsonList, "werk", "niet", "secreto", tickelijst);
-            bool loggedIn = false;
+            bool loggedIn = true;
             HoofdScherm hoofdScherm = new HoofdScherm();
-            hoofdScherm.GebruikerHoofdscherm(loggedIn);
-            loggedIn = true;
             hoofdScherm.GebruikerHoofdscherm(loggedIn);
         }
     }
--- projectB/hoofdScherm.cs
+++ projectB/hoofdScherm.cs
@@ -1,16 +1,37 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 
 public class HoofdScherm
 {
-	public void GebruikerHoofdscherm(bool loggedIn)
+	public string GebruikerHoofdscherm(bool loggedIn)
 	{
+        Console.Clear();
         string menu = "| [1] Film | [2] Locaties | [3] Eten & Drinken | [4] Zoeken | [5] Inloggen |";
+        string[] menuArr = new string[5] { "Films", "Locaties", "Eten & Drinken", "Zoeken", "Inloggen" };
+        string[] menuLenght = new string[5] { "1","2","3","4","5"};
         if (loggedIn)
         {
             menu = "| [1] Film | [2] Locaties | [3] Eten & Drinken | [4] Account Gegevens | [5] Zoeken | [6] Uitloggen |";
+            menuArr = new string[6] { "Films", "Locaties", "Eten & Drinken", "Account Gegevens", "Zoeken", "Uitloggen" };
+            menuLenght = new string[6] { "1", "2", "3", "4", "5","6" };
         }
         Console.WriteLine(menu);
-
+        string Input = "";
+        while (!menuLenght.Contains(Input))
+        {
+            Console.Clear();
+            Console.WriteLine(menu);
+            Console.WriteLine("Geef input:");
+            Input = Console.ReadLine();
+            if (!menuLenght.Contains(Input))
+            {
+                Console.WriteLine("Invalide Input!");
+            }
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine(menuArr[Int32.Parse(Input) - 1]);
+        return menuArr[Int32.Parse(Input)-1];
     }
     //public static string AdminHoofdscherm()
     //{
